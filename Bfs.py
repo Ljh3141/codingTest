0be054a780c5853569cd48590cdc0b357fc6a800
Ljh3141 <crido3141@gmail.com>
2023-05-29 17:00:39 +0900

    pccp 남은것들

---
+++ Bfs.py
@@ -0,0 +1,20 @@
+from collections import deque
+def solution(s,e):
+    answer = 0
+    ch = [0]*10001
+    dQ = deque()
+    dQ.append(s)
+    ch[s]=1
+    L = 0
+    while(dQ):
+        length = len(dQ)
+        for _ in range(length):
+            x=dQ.popleft()
+            if x==e:
+                return L
+            for nx in[x-1, x+1, x+5]:
+                if nx > 0 and nx < 10001 and ch[nx] == 0:
+                    ch[n] = 1
+                    dQ.append(nx)
+        L+=1
+    return answer
---
+++ Example.java
@@ -0,0 +1,6 @@
+public class Example {
+    public static void main(String[] args){
+        Integer a = null;
+        System.out.println("yay"+7*a);
+    }
+}
